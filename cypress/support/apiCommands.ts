6ca1025ae2f9d06ad2b94dbc13b5e917995db5f0
Aekachai <sniperaek@gmail.com>
2021-02-10 16:51:14 +0700

    Base structure with typescript

---
+++ cypress/support/apiCommands.ts
@@ -0,0 +1,25 @@
+/// <reference types="cypress" />
+declare namespace Cypress {
+  interface Chainable {
+    loginApi(email: string, password: string, statusCode: number, key: string, message: string): Chainable<Element>
+  }
+}
+  
+  
+Cypress.Commands.add(
+  'loginApi',
+  (email, password, statusCode, key, message) => {
+    cy.request({
+      method: 'POST',
+      url: 'api.mocki.io/v1/877a8df5',
+      body: {
+        email: email,
+        password: password,
+      },
+      failOnStatusCode: false,
+    }).then(response => {
+      expect(response.status).to.eq(statusCode)
+      expect(response.body).to.have.property(key, message)
+    })
+  },
+)
---
+++ cypress/support/commands.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+declare namespace Cypress {
+  interface Chainable {
+   visitGoogle(value: string): Chainable<Element>
+  }
+}
+  
+  
+  Cypress.Commands.add("visitGoogle", (user) => { 
+    cy.title().should('eq', user)
+ })
