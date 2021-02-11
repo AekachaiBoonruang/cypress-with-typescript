b6e21a02bf9aa5b7e3ab0d655f5d57ab9ccedbee
Aekachai <sniperaek@gmail.com>
2021-02-11 16:09:23 +0700

    Added generate report video

---
+++ cypress/integration/examples/actions3.spec.ts
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+describe('Sample cypress with typescript3', () => {
+
+  beforeEach(() => {
+    cy.visit('https://www.google.com/')
+  })
+
+  it('Visit google.com', () => {
+    const search: string = 'Google'
+    cy.visitGoogle(search)
+    cy.loginApi('arbotenacustomer@gmail.com', '20Scoops#',200, 'message','successed')
+  })
+
+})
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,21 +1,19 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
 import './commands.ts'
 import './apiCommands'
+const addContext = require('mochawesome/addContext')
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+Cypress.Screenshot.defaults({
+  screenshotOnRunFailure: false,
+  capture: 'fullPage',
+})
+
+
+Cypress.on('test:after:run', (test) => {
+    
+    addContext(     
+      {
+        test,
+      },
+      `assets/videos/${Cypress.spec.name}.mp4`,
+    )
+  })
