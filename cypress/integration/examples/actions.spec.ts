7e73b0c6a5864b05f6b0f733471e924154d85879
AekachaiBoonruang <sniperaek@gmail.com>
2021-05-14 21:00:22 +0700

    update text

--- cypress/integration/examples/actions.spec.ts
+++ cypress/integration/examples/actions.spec.ts
@@ -6,7 +6,7 @@ describe('Sample cypress with typescript', () => {
     cy.visit('https://www.google.com/')
   })
 
-  it('Visit google.com', () => {
+  it('Visit to google.com', () => {
     const search: string = 'Google'
     cy.visitGoogle(search)
     cy.loginApi('arbotenacustomer@gmail.com', '20Scoops#',200, 'message','successed')
