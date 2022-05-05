376d544fa32be474376077e8b37783432d86e66e
AekachaiBoonruang <sniperaek@gmail.com>
2022-05-05 15:16:20 +0700

    Add run different environment

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,21 @@
 const cypressTypeScriptPreprocessor = require("./cy-ts-preprocessor");
+const fs = require('fs-extra')
+const path = require('path')
 
 module.exports = (on) => {
   on("file:preprocessor", cypressTypeScriptPreprocessor);
 };
+
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve('cypress/', 'config', `${file}.json`)
+
+  return fs.readJson(pathToConfigFile)
+}
+
+module.exports = (on, config) => {
+  // accept a configFile value or use dev by default
+  const file = config.env.configFile || 'develop'
+
+  return getConfigurationByFile(file)
+}
